93bf04913bfb0f9d750e511d4081c9936adad958
Richard Tingstad <richard.tingstad@gmail.com>
2017-07-06 12:44:21 +0200

    WIP test

--- test.sh
+++ test.sh
@@ -63,6 +63,76 @@ testUsages() {
     assertEquals '' "$(echo -e "id1\tid2\t1\nid2\tid1\t1" | sort)" "$(cat "$WD/deps.tsv" | sort)"
 }
 
+testNoParameters() {
+    local out="$(main 2>&1)"
+
+    assertEquals 'Should print usage' "Usage:" "$(echo "$out" | cut -d' ' -f1)"
+}
+
+testMvnDependencyTreeOneModule() {
+    local dir="$(mktemp -d)"
+    mkdir -p "$dir/module1"
+    WD="$dir"
+    TARGET_DIR="$dir"
+    cat <<- EOF > "$TARGET_DIR/module1/pom.xml"
+		<project>
+			<modelVersion>4.0.0</modelVersion>
+			<groupId>g</groupId>
+			<artifactId>a</artifactId>
+			<version>1</version>
+		</project>
+	EOF
+
+    dependency-tree
+
+    read -r -d '' expected <<- TIL
+		digraph "g:a:jar:1" { 
+		 }
+	TIL
+    assertEquals "$expected " "$(cat "$WD/mvn.dot")"
+}
+
+testMvnDependencyTreeTwoModules() {
+    local dir="$(mktemp -d)"
+    mkdir -p "$dir/module1"
+    mkdir -p "$dir/module2"
+    WD="$dir"
+    TARGET_DIR="$dir"
+    cat <<- EOF > "$TARGET_DIR/module1/pom.xml"
+		<project>
+			<modelVersion>4.0.0</modelVersion>
+			<groupId>g</groupId>
+			<artifactId>a</artifactId>
+			<version>1</version>
+		</project>
+	EOF
+    cat <<- EOF > "$TARGET_DIR/module2/pom.xml"
+		<project>
+			<modelVersion>4.0.0</modelVersion>
+			<groupId>g</groupId>
+			<artifactId>b</artifactId>
+			<version>1</version>
+			<dependencies><dependency>
+				<groupId>g</groupId>
+				<artifactId>a</artifactId>
+				<version>1</version>
+				<type>pom</type>
+			</dependency></dependencies>
+		</project>
+    
+	EOF
+
+    dependency-tree
+
+    read -r -d '' expected <<- TIL
+		digraph "g:a:jar:1" { 
+		} digraph "g:b:jar:1" { 
+		       "g:b:jar:1" -> "g:a:jar:1" ;
+		 }
+	TIL
+    assertEquals "$expected " "$(cat "$WD/mvn.dot")"
+}
+
 testUsages2() {
     local src1="$(mktemp -d)"
     mkdir -p "$src1/src/com/foo/package2" 
@@ -72,7 +142,7 @@ testUsages2() {
     TARGET_DIR="$src1"
     echo -e "package com.foo.package2;\nimport com.foo.package4.Two;public class One{} " \
         > "$src1/src/com/foo/package2/One.java"
-    echo -e "package com.foo.package4;\nimport com.foo.package2.One;public class Two{} " \
+    echo -e "package com.foo.package4;\npublic class Two{} " \
         > "$src2/src/com/foo/package4/Two.java"
     echo -e "id1\tjar\tpom.xml\t$src1\t$src1/src\t$src1/src" > "$WD/modules.tab"
     echo -e "id2\tjar\tpom.xml\t$src2\t$src2/src\t$src2/src" >>"$WD/modules.tab"
@@ -87,11 +157,10 @@ testUsages2() {
     read -r -d '' expected <<- TIL
 		id1	com/foo/package2/One.java	com.foo.package2
 		id1	com/foo/package2/One.java	com.foo.package4
-		id2	com/foo/package4/Two.java	com.foo.package2
 		id2	com/foo/package4/Two.java	com.foo.package4
 	TIL
     assertEquals '' "$(echo -e "$expected" | sort)" "$(cat "$WD/deps-detailed.tsv" | sort)"
-    assertEquals '' "$(echo -e "id1\tid2\t1\nid2\tid1\t1" | sort)" "$(cat "$WD/deps.tsv" | sort)"
+    assertEquals '' "$(echo -e "id1\tid2\t1" | sort)" "$(cat "$WD/deps.tsv" | sort)"
 }
 
 DIR="$( dirname "$(pwd)/$0" )"
